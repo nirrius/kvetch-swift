ded216e688982f56f29e61d584f8063588b4771c
Teffen Ellis (Eric) <teffen@nirri.us>
2015-05-10 15:16:32 -0700

    Initial Commit

---
+++ kvetch/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  kvetch
+//
+//  Created by Teffen Ellis on 5/10/15.
+//  Copyright (c) 2015 Nirrius. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
