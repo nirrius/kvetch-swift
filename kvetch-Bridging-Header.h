ea499b70cc896a406b60e26543bf4e0449a5fd82
Jonah Stiennon <jonahss@gmail.com>
2015-05-10 18:52:48 -0700

    mm this is some stuff?

---
+++ kvetch-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  kvetch-Bridging-Header.h
+//  kvetch
+//
+//  Created by Jonah Stiennon on 5/10/15.
+//  Copyright (c) 2015 Nirrius. All rights reserved.
+//
+
+#ifndef kvetch_kvetch_Bridging_Header_h
+#define kvetch_kvetch_Bridging_Header_h
+
+#import <Parse/PFObject+Subclass.h>
+
+#endif
