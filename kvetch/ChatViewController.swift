21e745dbf50c677a9bbc1b2952d2204466edf640
Jonah Stiennon <jonahss@gmail.com>
2015-05-23 13:40:47 -0700

    silly table views

--- kvetch/ChatViewController.swift
+++ kvetch/ChatViewController.swift
@@ -23,13 +23,36 @@ class ChatViewController: UIViewController {
         logInButton.center = self.view.center
         self.view.addSubview(logInButton)
 
-        
-        
-        let testObject = PFObject(className: "TestObject")
-        testObject["foo"] = "bar"
-        testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
-            println("Object has been saved.")
+        func addTable(x : Float, y: Int , w : Int, h : Int, color: UIColor) {
+            var rect = CGRectMake(CGFloat(x), CGFloat(y), CGFloat(w), CGFloat(h))
+            var table = UITableView(frame: rect, style: UITableViewStyle.Plain)
+            table.backgroundColor = color
+            self.view.addSubview(table)
         }
+        
+    
+        addTable(0, 0, 10, 10, UIColor.redColor())
+        addTable(10, 10, 10, 10, UIColor.blueColor())
+        addTable(20, 20, 10, 10, UIColor.greenColor())
+        addTable(30, 30, 20, 20, UIColor.redColor())
+        addTable(50, 50, 40, 40, UIColor.blueColor())
+        addTable(90, 90, 80, 80, UIColor.greenColor())
+        addTable(170, 170, 160, 160, UIColor.redColor())
+        addTable(230, 230, 320, 320, UIColor.blueColor())
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//        let testObject = PFObject(className: "TestObject")
+//        testObject["foo"] = "bar"
+//        testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+//            println("Object has been saved.")
+//        }
     }
 
     override func didReceiveMemoryWarning() {
